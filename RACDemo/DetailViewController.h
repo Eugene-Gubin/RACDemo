0b2bff2c039dcb33968651c6e324d3adc1400e4e
Евгений Губин <hemet.mail@gmail.com>
2014-11-10 15:45:48 +0300

    Initial Commit

---
+++ RACDemo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  RACDemo
+//
+//  Created by Евгений Губин on 10.11.14.
+//  Copyright (c) 2014 simbirsoft.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
