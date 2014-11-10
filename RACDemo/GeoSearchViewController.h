db87f65dc590e44c811a4abccb2f250d0691de41
Евгений Губин <hemet.mail@gmail.com>
2014-11-10 17:57:37 +0300

    First commit.

---
+++ RACDemo/GeoSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RACDemo
+//
+//  Created by Евгений Губин on 10.11.14.
+//  Copyright (c) 2014 simbirsoft.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GeoSearchViewController : UIViewController
+
+@end
---
+++ RACDemo/GeoSearchViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  RACDemo
+//
+//  Created by Евгений Губин on 10.11.14.
+//  Copyright (c) 2014 simbirsoft.com. All rights reserved.
+//
+
+#import "GeoSearchViewController.h"
+
+@interface GeoSearchViewController ()
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GeoSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
